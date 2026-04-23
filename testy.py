c0d22cbc88bcefa25881e5c5ba9f77df52fecca0
avramkinostap <av.ostap13@gmail.com>
2026-04-23 09:42:54 +0200

    Update testy.py

--- testy.py
+++ testy.py
@@ -2,3 +2,14 @@ import unittest
 from kalkulator import calculate
 
 class TestKalkulator(unittest.TestCase):
+    def test_dodawanie(self):
+        self.assertEqual(calculate("2+2"), 4)
+
+    def test_odejmowanie(self):
+        self.assertEqual(calculate("10-3"), 7)
+
+    def test_mnozenie(self):
+        self.assertEqual(calculate("3*4"), 12)
+
+    def test_dzielenie(self):
+        self.assertEqual(calculate("10/2"), 5.0)
