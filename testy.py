ae6d8ec820c201bce46e577e307d8c17d749ee8d
avramkinostap <av.ostap13@gmail.com>
2026-04-23 22:17:12 +0200

    Update testy.py

--- testy.py
+++ testy.py
@@ -13,3 +13,17 @@ class TestKalkulator(unittest.TestCase):
 
     def test_dzielenie(self):
         self.assertEqual(calculate("10/2"), 5.0)
+    def test_potega(self):
+        self.assertEqual(calculate("2^3"), 8)
+
+    def test_pierwiastek(self):
+        self.assertEqual(calculate("math.sqrt(9)"), 3.0)
+
+    def test_procent(self):
+        self.assertEqual(calculate("50%"), 0.5)
+
+    def test_procent_dodawanie(self):
+        self.assertEqual(calculate("200+10%"), 220.0)
+
+    def test_procent_odejmowanie(self):
+        self.assertEqual(calculate("200-10%"), 180.0)
